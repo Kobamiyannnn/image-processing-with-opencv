bb6a28073624384b81a6b6c8ef7991aa4b3d61c1
Kobamiyannnn <kobaprfm23@gmail.com>
2022-11-22 20:27:24 +0900

    基本編4途中まで

--- src/basic-chap/4_separate_img_into_rgb.py
+++ src/basic-chap/4_separate_img_into_rgb.py
@@ -2,3 +2,12 @@
 # 4. カラー画像を読み込み，3チャネル（RGB）に分離し，入力画像と共に各チャネルの画像を表示するプログラム
 
 import cv2
+
+img = cv2.imread("../../data/Lenna.jpg")
+
+# 3チャネルに分離する
+# 0: Blue, 1: Green, 2: Red
+img_r = img_g = img_b = img.copy()
+img_r[:, :, 0] = img_r[:, :, 1] = 0
+img_g[:, :, 0] = img_g[:, :, 2] = 0
+img_b[:, :, 1] = img_b[:, :, 2] = 0
