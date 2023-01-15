b3719b2f67d9afc7dc0318f2be74f930588cb767
Kobamiyannnn <kobaprfm23@gmail.com>
2023-01-15 21:41:57 +0900

    画像処理編15完成

--- src/image-processing-chap/15_otsu_method.py
+++ src/image-processing-chap/15_otsu_method.py
@@ -1,40 +1,11 @@
 # 画像処理編
-# 大津の二値化により、画像を二値化するプログラム
-
-# http://labs.eecs.tottori-u.ac.jp/sd/Member/oyamada/OpenCV/html/py_tutorials/py_imgproc/py_thresholding/py_thresholding.html
+# 15. 大津の二値化により、画像を二値化するプログラム
 
 import cv2
-import numpy as np
 
 img = cv2.cvtColor(cv2.imread("../../data/Lenna.jpg"), cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(img, (5, 5), 0)
 
-# find normalized_histogram, and its cumulative distribution function
-hist = cv2.calcHist([blur], [0], None, [256], [0, 256])
-hist_norm = hist.ravel() / hist.max()
-Q = hist_norm.cumsum()
-
-bins = np.arange(256)
-
-fn_min = np.inf
-thresh = -1
-
-for i in range(1, 256):
-    p1, p2 = np.hsplit(hist_norm, [i]) # probabilities
-    q1, q2 = Q[i], Q[255] - Q[i] # cum sum of classes
-    b1, b2 = np.hsplit(bins, [i]) # weights
-
-    # finding means and variances
-    m1, m2 = np.sum(p1 * b1) / q1, np.sum(p2 * b2) / q2
-    v1, v2 = np.sum(((b1 - m1) ** 2) * p1) / q1, np.sum(((b2 - m2) ** 2) * p2) / q2
-
-    # calculates the minimization function
-    fn = v1 * q1 + v2 * q2
-    if fn < fn_min:
-        fn_min = fn
-        thresh = i
-
-# find otsu's threshold value with OpenCv function
 th_val, processed_img = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-print(thresh, th_val)
+cv2.imwrite("../../data/image-processing-chap/ip_15.jpg", processed_img)
